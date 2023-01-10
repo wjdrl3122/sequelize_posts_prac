ada9b90e364d2113e4a14a8894968615879e8f2b
wjdrl3122 <wjdrl3122@gmail.com>
2023-01-10 14:48:08 +0900

    add sequelize setting

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const { sequelize } = require('./models'); 
+
 const app = express();
 
 // env setting
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, console.log("Server don start for port:" + PORT));
+app.listen(PORT, async() => {
+    console.log("Server don start for port:" + PORT);
+    await sequelize.authenticate();
+    console.log("db authenticated");
+});
